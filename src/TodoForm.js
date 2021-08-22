553e12f027f03829eddc0b5e8c43ff09c63fe312
abfrogosa <abfrogosa@iskolarngbayan.edu.ph>
2021-08-22 11:26:13 +0800

    new descriptioon field added

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -3,7 +3,10 @@ import { useForm } from "react-hook-form";
 
 export const TodoForm = ({ todo, onSubmit }) => {
   const { register, handleSubmit } = useForm({
-    defaultValues: { text: todo ? todo.text : "" },
+    defaultValues: { 
+      text: todo ? todo.text : "",
+      description: todo ? todo.description : ""
+      },
   });
 
   const submitHandler = handleSubmit((data) => {
@@ -15,6 +18,8 @@ export const TodoForm = ({ todo, onSubmit }) => {
       <div className="form-group">
         <label htmlFor="text">Text:</label>
         <input {...register('text')} className="form-control" type="text" name="text" id="text" />
+        <label htmlFor="description">Description:</label>
+        <input {...register('description')} className="form-control" type="text" name="description" id="description" />
       </div>
       <div className="form-group">
         <button type="submit" className="btn btn-primary">
