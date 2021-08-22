9312ffa15d9087bdd4523bfa5fbaa4662fa51316
abfrogosa <abfrogosa@iskolarngbayan.edu.ph>
2021-08-22 11:02:53 +0800

    route path added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
 import { TodoList } from './TodoList'
 import { CreateTodo } from './CreateTodo'
 import { EditTodo } from './EditTodo'
+
 
 function App() {
   return (
@@ -17,11 +19,11 @@ function App() {
           </li>
         </ul>
       </nav>
-    <Switch>
-      <Route exact path="/" component={TodoList}/>
-      <Route path="/edit/:id" component={EditTodo}/>
-      <Route path="/create" component={CreateTodo}/>
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={TodoList} />
+        <Route exact path="/edit/:id" component={EditTodo} />
+        <Route exact path="/create" component={CreateTodo} />
+      </Switch>
     </div>
   );
 }
